e2a333975f275066aacd6189d351153b7969ddc7
Jackson Davis <jackson@jcdav.is>
2025-12-01 20:22:16 -0800

    d1p2

--- day1/day1.py
+++ day1/day1.py
@@ -11,12 +11,22 @@ def main():
         for line in file.readlines():
             num = int(line[1:])
             dir = -1 if line[0] == 'L' else 1
+            zeroes += num // 100
+            num %= 100
             position += num*dir
-            position = ((position%100)+100)%100
-            #print(f'After {line} at {position}')
             if position == 0:
-                #print('Zero!')
                 zeroes += 1
+            elif position > 0:
+                if position >= 100:
+                    zeroes += 1
+                    position -= 100
+            else:
+                if dir*num == position:
+                    zeroes -= 1
+                if position < 0:
+                    zeroes += 1
+                    position += 100
+
     print(zeroes)
 
 if __name__ == "__main__":
