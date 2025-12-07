a7367371e55c2f8b721b05ccd26723745322e7b0
Jackson Davis <jackson@jcdav.is>
2025-12-06 21:10:34 -0800

    d7p2

--- day7/day7.py
+++ day7/day7.py
@@ -4,21 +4,28 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('file')
     args = parser.parse_args()
-    sum = 0
+
     with open(args.file) as file:
         data = file.readlines()
-        beams = set([data[0].find('S')])
-        i = 1
-        for i in range(1, len(data)):
-            for j in range(len(data[i])):
-                if data[i][j] == '^':
-                    if j in beams:
-                        beams.remove(j)
-                        beams.add(j-1)
-                        beams.add(j+1)
-                        sum += 1
+        start = data[0].find('S')
+        cache = {}
 
-    print(sum)
+        def recurse(row: int, col: int) -> int:
+            if col < 0 or col > len(data[0]):
+                return 0
+            if row == len(data):
+                return 1
+            if (row, col) in cache:
+                return cache[(row, col)]
+            result = -1
+            if data[row][col] == '^':
+                result = recurse(row+1, col-1) + recurse(row+1, col+1)
+            else:
+                result = recurse(row+1, col)
+            cache[(row, col)] = result
+            return result
+
+        print(recurse(1, start))
 
 if __name__ == "__main__":
     main()
